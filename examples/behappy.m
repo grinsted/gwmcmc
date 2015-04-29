89e854863264d0bb408657cff80d0fcf61e05b52
Aslak Grinsted <ag@glaciology.net>
2015-04-29 23:13:19 +0200

    Adding be happy example

---
+++ examples/behappy.m
@@ -0,0 +1,34 @@
+%% Don't worry, be Happy
+%
+% Sampling a smiley face likelihood function.
+% 
+
+addpath ..  %first we add the path of the toolbox folder 
+
+%% Smiley face equation
+% Formulate a likelihood function inspired by an equation of a smiley face.
+%  
+% Source Michael Borcherds: https://twitter.com/mike_geogebra/status/135391208703930369
+
+logHappiness=@(m)1-exp(1e-4*((m(1)^4+2*m(1)^2*m(2)^2-0.3*m(1)^2*m(2)-40.75*m(1)^2+m(2)^4-m(2)^3-40.75*m(2)^2+25*m(2)+393.75)*((m(1)+3)^2+(m(2)-7)^2-1)*((m(1)-3)^2+(m(2)-7)^2-1)*(m(1)^2+(m(2)-2)^2-64)));
+
+
+%% Draw samples from the distribution using GWMCMC
+%
+% Now we apply the MCMC hammer to draw samples from the logHappiness distribution.
+%
+
+[models,logP]=gwmcmc(randn(2,100),logHappiness,100000,'ThinChain',2);
+models(:,:,1:end*.2)=[];
+models=models(:,:)';
+
+
+plot(models(:,1),models(:,2),'r.');
+
+axis equal off 
+
+
+title('GWMCMC says: "Don''t Worry, Be Happy!"')
+
+
+% Bobby McFerrin on youtube: https://www.youtube.com/watch?v=d-diB65scQU
